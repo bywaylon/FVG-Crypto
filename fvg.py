95a18f41d4323b7752380101ba52bebb1795e8b9
bywaylon <waylondermody68@gmail.com>
2026-07-14 10:28:47 +0000

    Added setup scoring system

--- fvg.py
+++ fvg.py
@@ -1,63 +1,60 @@
-from binance.client import Client
-import pandas as pd
-
-client = Client()
-
-
-def get_candles(symbol, interval="1h"):
-
-    candles = client.get_klines(
-        symbol=symbol,
-        interval=interval,
-        limit=100
-    )
-
-    df = pd.DataFrame(
-        candles,
-        columns=[
-            "time",
-            "open",
-            "high",
-            "low",
-            "close",
-            "volume",
-            "x1",
-            "x2",
-            "x3",
-            "x4",
-            "x5",
-            "x6"
-        ]
-    )
-
-    df["high"] = df["high"].astype(float)
-    df["low"] = df["low"].astype(float)
-    df["close"] = df["close"].astype(float)
-
-    return df
+from indicators import get_trend, volume_strength, fvg_size
 
 
 def check_fvg(symbol):
 
     df = get_candles(symbol)
 
-    last = df.iloc[-1]
-    two_back = df.iloc[-3]
+    score = 0
+    reasons = []
+
+    trend = get_trend(df)
+
+    size = fvg_size(df)
+
+    volume = volume_strength(df)
 
 
-    # Bullish FVG
-    if last["low"] > two_back["high"]:
+    if trend == "Bullish":
+        score += 25
+        reasons.append("Bullish trend")
+
+    else:
+        score += 25
+        reasons.append("Bearish trend")
+
+
+    if size > 0.1:
+        score += 25
+        reasons.append("Large FVG")
+
+
+    if volume > 1.3:
+        score += 25
+        reasons.append("Volume increase")
+
+
+    if df["low"].iloc[-1] > df["high"].iloc[-3]:
+
+        score += 25
+
         return {
             "coin": symbol.replace("USDT",""),
-            "type": "Bullish FVG"
+            "direction": "LONG",
+            "score": score,
+            "reasons": reasons
         }
 
 
-    # Bearish FVG
-    if last["high"] < two_back["low"]:
+    if df["high"].iloc[-1] < df["low"].iloc[-3]:
+
+        score += 25
+
         return {
             "coin": symbol.replace("USDT",""),
-            "type": "Bearish FVG"
+            "direction": "SHORT",
+            "score": score,
+            "reasons": reasons
         }
 
 
