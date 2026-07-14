60b7d8fb4cee7c36078cb9328c943b4517ff34b9
bywaylon <waylondermody68@gmail.com>
2026-07-14 10:26:31 +0000

    added trading indicators

--- indicators.py
+++ indicators.py
@@ -1,6 +1,35 @@
-# This file will contain trading indicators
-# Example:
-# volume
-# EMA
-# trend
-# volatility
+import pandas as pd
+
+
+def get_trend(df):
+
+    ema = df["close"].ewm(span=50).mean()
+
+    if df["close"].iloc[-1] > ema.iloc[-1]:
+        return "Bullish"
+
+    else:
+        return "Bearish"
+
+
+
+def volume_strength(df):
+
+    current = df["volume"].iloc[-1]
+
+    average = df["volume"].mean()
+
+    return current / average
+
+
+
+def fvg_size(df):
+
+    size = abs(
+        df["low"].iloc[-1] -
+        df["high"].iloc[-3]
+    )
+
+    price = df["close"].iloc[-1]
+
+    return (size / price) * 100
