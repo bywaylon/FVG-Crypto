def545fd3aa15ba03906b066b1fa0cc90e69c585
bywaylon <waylondermody68@gmail.com>
2026-07-14 10:10:19 +0000

    added binance market scanner

--- scanner.py
+++ scanner.py
@@ -1,1 +1,26 @@
-# This file will collect crypto market data
+from binance.client import Client
+import pandas as pd
+
+client = Client()
+
+
+def get_top_coins():
+
+    tickers = client.get_ticker()
+
+    df = pd.DataFrame(tickers)
+
+    # Only USDT pairs
+    df = df[df["symbol"].str.endswith("USDT")]
+
+    # Convert volume to numbers
+    df["quoteVolume"] = pd.to_numeric(df["quoteVolume"])
+
+    # Sort by volume
+    df = df.sort_values(
+        "quoteVolume",
+        ascending=False
+    )
+
+    # Return top 100 coins
+    return df.head(100)["symbol"].tolist()
